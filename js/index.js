1a81d13f07924da4ec3ef082a56eb5a30959d481
Leonardo Galindez <88455461+Leonardo-Galindez@users.noreply.github.com>
2024-09-16 23:13:18 -0300

    cambio del diseño de el home

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const target = document.querySelector(`.${button.dataset.target}`);
+
+    target.classList.toggle("active");
+  });
+});
