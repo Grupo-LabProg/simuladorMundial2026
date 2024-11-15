5f0d248d69bd4154bcd2ecaf7206cff72ead1d21
Leonardo Galindez <88455461+Leonardo-Galindez@users.noreply.github.com>
2024-11-14 22:35:09 -0300

    cambios

---
+++ client/src/modules/confederaciones.js
@@ -0,0 +1,35 @@
+export function insertarTablaConfederacion(confederacion, equipos) {
+  console.log(confederacion);
+
+  const confederacionesDiv = document.querySelector(".faseConfederaciones");
+  console.log(confederacionesDiv);
+
+  let html = `
+        <div class="tabla-confederacion">
+            <h3>Confederación: ${confederacion}</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Equipo</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+
+  equipos.forEach((equipo, index) => {
+    html += `
+            <tr>
+                <td>${index + 1}</td>
+                <td class="team"><img src="${equipo.flag}" alt="${
+      equipo.name
+    }" width="20"> ${equipo.name}</td>
+            </tr>`;
+  });
+
+  html += `</tbody>
+            </table>
+        </div>`;
+
+  // Inserta la tabla en la sección de confederaciones
+  confederacionesDiv.insertAdjacentHTML("beforeend", html);
+}
