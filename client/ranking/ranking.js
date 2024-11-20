7c423c306599ad91c99c2abea329aa505c0f3582
KevinQuintero <kevin.quintero@est.fi.uncoma.edu.ar>
2024-11-19 21:02:10 -0300

    Arreglando confederaciones para que aparezcan una vez que le das click desde otra ventana

--- client/ranking/ranking.js
+++ client/ranking/ranking.js
@@ -126,3 +126,14 @@ function insertHeader() {
   const headerContainer = document.getElementById("header");
   headerContainer.innerHTML = headerHTML1;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("link")) {
+      //event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+      const target = event.target.dataset.target; // Obtengo el valor de data-target
+      localStorage.setItem("targetSection", target); // Guarda el valor en localStorage
+      // window.location.href = event.target.href; // Redirige a la página principal
+    }
+  });
+});
